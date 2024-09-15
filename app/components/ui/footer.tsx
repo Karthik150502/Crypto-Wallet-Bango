907f9a3f8e9376ea2d722346f9c041265e0ecc85
Karthik150502 <karthikrdycool@gmail.com>
2024-09-15 16:17:58 +0530

    Made the footer transparent

--- app/components/ui/footer.tsx
+++ app/components/ui/footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export default function Footer() {
     return (
-        <div className='h-[40px] text-center text-xs absolute bottom-0 right-0 px-4 bg-black flex items-center justify-between
+        <div className='h-[40px] text-center text-xs absolute bottom-0 right-0 px-4 bg-transparent flex items-center justify-between
         '>
             <p>Copyright &#169; Bango, All right reserved.</p>
         </div>
